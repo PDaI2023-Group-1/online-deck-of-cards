7065165ad8c75a13f2d77636ef805bc0cb7efdb9
Lasse Suomela <suomelasse@gmail.com>
2023-11-24 17:20:33 +0200

    Fixed styling

--- frontend/src/components/Rooms/WaitingRoom.tsx
+++ frontend/src/components/Rooms/WaitingRoom.tsx
@@ -4,7 +4,6 @@ import axios from 'axios';
 import { writeClipboard } from '@solid-primitives/clipboard';
 import WSClient from '../../API/WSClient';
 import { jwtDecode } from 'jwt-decode';
-import { PlayerChanged } from '../../../types/custom';
 
 const WaitingRoom: Component = () => {
     const navigate = useNavigate();
@@ -132,72 +131,66 @@ const WaitingRoom: Component = () => {
 
     return (
         <div class="flex flex-col justify-center items-center h-screen">
-            <div class="flex justify-center items-center">
-                <div class="flex flex-col justify-center items-center mr-8">
-                    <form>
-                        <label class="block text-gray-700 text-sm font-bold mb-2">
-                            How many decks: {deckCount()}
-                        </label>
-                        <input
-                            type="range"
-                            class={`${
-                                isOwner()
-                                    ? 'cursor-pointer'
-                                    : 'disabled:opacity-75 cursor-not-allowed'
-                            }`}
-                            min="1"
-                            max="2"
-                            value={deckCount()}
-                            onInput={(e) =>
-                                setDeckCount(parseInt(e.currentTarget.value))
-                            }
-                            disabled={!isOwner()}
-                        />
-                        <label class="block text-gray-700 text-sm font-bold mb-2">
-                            How many cards are dealt to players:{' '}
-                            {cardsPerPlayer()}
-                        </label>
-                        <input
-                            type="range"
-                            class={`${
-                                isOwner()
-                                    ? 'cursor-pointer'
-                                    : 'disabled:opacity-75 cursor-not-allowed'
-                            }`}
-                            min="0"
-                            max={
-                                (deckCount() * 52 + jokerCount()) /
-                                maxPlayerCount()
-                            }
-                            value={cardsPerPlayer()}
-                            onInput={(e) =>
-                                setCardsPerPlayer(
-                                    parseInt(e.currentTarget.value),
-                                )
-                            }
-                            disabled={!isOwner()}
-                        />
-                        <label class="block text-gray-700 text-sm font-bold mb-2">
-                            How many jokers are in game: {jokerCount()}
-                        </label>
-                        <input
-                            type="range"
-                            class={`${
-                                isOwner()
-                                    ? 'cursor-pointer'
-                                    : 'disabled:opacity-75 cursor-not-allowed'
-                            }`}
-                            min="0"
-                            max={deckCount() * 4}
-                            value={jokerCount()}
-                            onInput={(e) =>
-                                setJokerCount(parseInt(e.currentTarget.value))
-                            }
-                            disabled={!isOwner()}
-                        />
-                    </form>
-                </div>
-                <div class="border-r border-blue-400 h-40" />
+            <div class="flex">
+                <form>
+                    <label class="block text-gray-700 text-sm font-bold mb-2">
+                        How many decks: {deckCount()}
+                    </label>
+                    <input
+                        type="range"
+                        class={`${
+                            isOwner()
+                                ? 'cursor-pointer'
+                                : 'disabled:opacity-75 cursor-not-allowed'
+                        }`}
+                        min="1"
+                        max="2"
+                        value={deckCount()}
+                        onInput={(e) =>
+                            setDeckCount(parseInt(e.currentTarget.value))
+                        }
+                        disabled={!isOwner()}
+                    />
+                    <label class="block text-gray-700 text-sm font-bold mb-2">
+                        How many cards are dealt to players: {cardsPerPlayer()}
+                    </label>
+                    <input
+                        type="range"
+                        class={`${
+                            isOwner()
+                                ? 'cursor-pointer'
+                                : 'disabled:opacity-75 cursor-not-allowed'
+                        }`}
+                        min="0"
+                        max={
+                            (deckCount() * 52 + jokerCount()) / maxPlayerCount()
+                        }
+                        value={cardsPerPlayer()}
+                        onInput={(e) =>
+                            setCardsPerPlayer(parseInt(e.currentTarget.value))
+                        }
+                        disabled={!isOwner()}
+                    />
+                    <label class="block text-gray-700 text-sm font-bold mb-2">
+                        How many jokers are in game: {jokerCount()}
+                    </label>
+                    <input
+                        type="range"
+                        class={`${
+                            isOwner()
+                                ? 'cursor-pointer'
+                                : 'disabled:opacity-75 cursor-not-allowed'
+                        }`}
+                        min="0"
+                        max={deckCount() * 4}
+                        value={jokerCount()}
+                        onInput={(e) =>
+                            setJokerCount(parseInt(e.currentTarget.value))
+                        }
+                        disabled={!isOwner()}
+                    />
+                </form>
+                <div class="border-r border-blue-400 h-40 ml-10 mr-10" />
                 <div class="flex flex-col justify-center items-center ml-8">
                     <div class="bg-white rounded-lg shadow-lg p-6">
                         <p class="text-lg font-bold mb-2">
